92da3f74fe22ffc61d9c12cebed680e4c7fde52b
Reinout van Rees <reinout@vanrees.org>
2014-05-01 12:44:16 +0200

    Basic version that finds files. Not functional yet.

--- README.rst
+++ README.rst
@@ -1,4 +1,19 @@
 Geoserver migration helper script
 =================================
 
-TODO
+Give a directory to this script and it'll search the directories for
+``datastore.xml`` files and adjust two things, if found:
+
+- A database host that starts with ``s-`` is replaced with one starting with
+  ``p-``: we're moving to production.
+
+- The line with the password is emptied. This forces geoserver to ask you for
+  the password again. Simply copy/pasting the password between different
+  geoservers doesn't work, probably because the encrypted password is hashed
+  with some secret key or so.
+
+The script also prints out which files it changed and what it did in them.
+
+Usage:
+
+    $ python migrate.py some/fancy/directory
--- migrate.py
+++ migrate.py
@@ -1,4 +1,31 @@
+import logging
+import os
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+def datastore_filepaths(basedir):
+    """Return paths to all datastore.xml files found in basedir (+subdirs)."""
+    for (dirpath, dirnames, filenames) in os.walk(basedir):
+        if not 'datastore.xml' in filenames:
+            logger.debug("Directory %s doesn't contain a datastore.xml",
+                         dirpath)
+            continue
+        yield os.path.join(dirpath, 'datastore.xml')
+
+
 def main():
+    """Main method, called when you run the script."""
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(levelname)s: %(message)s')
+    if len(sys.argv) != 2:
+        logger.critical("Pass the script one directory")
+        sys.exit(1)
+    basedir = sys.argv[1]
+
+    for filepath in datastore_filepaths(basedir):
+        logger.info(filepath)
     print("hoera")
 
 
